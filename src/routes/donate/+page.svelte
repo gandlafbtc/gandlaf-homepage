18d4aad90579bae6bcfe87d8fd05f5b19588bd34
gandlaf21 <gandlaf@proton.me>
2023-03-02 15:37:32 +0100

    pretty donate page

--- src/routes/donate/+page.svelte
+++ src/routes/donate/+page.svelte
@@ -1,19 +1,25 @@
 <script>
 	import CashuTip from "../../comp/CashuTip.svelte";
-import LnTip from "../../comp/LNTip.svelte";
+    import LnTip from "../../comp/LNTip.svelte";
+    let isCashu = false 
 </script>
 <div class="flex flex-col w-full items-center justify-center pt-5 gap-2">
     <h1 class="font-bold text-xl">
         Support @gandlaf21
     </h1>
-    <div>
+    <div class="flex gap-1">
+        use Cashu: 
+        <input type="checkbox" class="toggle toggle-primary" bind:checked={isCashu} />
 
+    </div>
+    <div class="flex justify-center">
+        {#if isCashu}
+            <CashuTip lnaddress="0x0dd4e7be973f16a9@ln.tips"></CashuTip>
+        {:else}
         <div>
             <LnTip lnaddress="gandlaf21@stacker.news"></LnTip>
         </div>
-        <div>
-            <CashuTip lnaddress="0x0dd4e7be973f16a9@ln.tips"></CashuTip>
-        </div>
+        {/if}
     </div>
         
 </div>
